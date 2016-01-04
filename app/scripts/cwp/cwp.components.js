13d464b7b4291e59e73b253d12ebaa1a5bac47b1
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-04 18:06:46 +0100

    Add my-cwp component

---
+++ app/scripts/cwp/cwp.components.js
@@ -0,0 +1,32 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 4me.core.cwp.directives
+ * @description
+ * # CWP components
+ * CWP Components
+ */
+var cwpComponents = angular.module('4me.core.cwp.components', [
+  '4me.core.lodash',
+  '4me.core.cwp.services'
+]);
+
+cwpComponents.component('myCwp', {
+  restrict: 'E',
+  controller: myCwpController,
+  template: '{{myCwp.cwp}}'
+});
+
+myCwpController.$inject = ['myCwp'];
+function myCwpController(myCwp) {
+  var self = this;
+  self.cwp = '';
+  myCwp.get().then(function(res) {
+    self.cwp = res.name;
+  });
+}
+
+
+}());
--- app/scripts/cwp/cwp.js
+++ app/scripts/cwp/cwp.js
@@ -9,7 +9,8 @@
  * Meta module to include cwp components
  */
 angular.module('4me.core.cwp', [
-  '4me.core.cwp.services'
+  '4me.core.cwp.services',
+  '4me.core.cwp.components'
 ]);
 
 }());
