967289519f08a6a252ac6f443105648d5ee06791
kouak <benjamin.beret@gmail.com>
2016-01-04 10:33:54 +0100

    Clean up dependancies, add boilerplate index.html

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+(function() {
+'use strict';
+/**
+ * @ngdoc overview
+ * @name 4me.core.ui
+ * @description
+ * # 4me.core.ui
+ *
+ * Main module of the application.
+ */
+angular
+.module('4me.core.ui', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngSanitize',
+    'ngMaterial'
+]);
+
+}());
