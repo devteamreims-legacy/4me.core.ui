f6ef6045106ea7cc32ba215b10f422ac19a09ec9
kouak <benjamin.beret@gmail.com>
2016-01-05 14:26:38 +0100

    Add error management placeholders

---
+++ app/scripts/errors/errors.js
@@ -0,0 +1,15 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 4me.core.errors
+ * @description
+ * # Error management module
+ *
+ * Meta module to include error components/services
+ */
+angular.module('4me.core.errors', [
+]);
+
+}());
---
+++ app/scripts/errors/errors.services.js
@@ -0,0 +1,18 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 4me.core.errors.services
+ * @description
+ * # Errors Services
+ *
+ * Errors Services
+ */
+
+angular.module('4me.core.errors.services', [
+  '4me.core.config',
+  '4me.core.lodash'
+]);
+
+}());
