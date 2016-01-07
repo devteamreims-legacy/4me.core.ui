9d7c8cb7ff908e6d07e0b4d9d2a896574adbd85e
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-07 11:41:39 +0100

    Add error management service

--- app/scripts/errors/errors.js
+++ app/scripts/errors/errors.js
@@ -10,6 +10,51 @@
  * Meta module to include error components/services
  */
 angular.module('4me.core.errors', [
-]);
+  '4me.core.lodash'
+])
+.factory('errors', errors);
+
+errors.$inject = [];
+function errors() {
+  // Array to hold our errors
+  /** Error object prototype :
+   * {
+   *   sender: 'core' // String to hold our sender module
+   *   type: 'error' // String to hold our error type (critical, warning, info)
+   *   message: 'Invalid info from backend' // String to hold our user friendly info
+   *   reason: {} // Object with more detailed stuff
+   * }
+   */
+
+  var errors = [];
+  var service = {};
+
+  // Promise version of our error catcher
+  service.catch = function(sender, type, message) {
+    return function(reason) {
+      service.add(sender, type, message, reason);
+    };
+  };
+
+  // Add an error
+  service.add = function(sender, type, message, reason) {
+    var e = {
+      sender: sender || 'Unknown',
+      type: type || 'warning',
+      message: message || 'Unknown message',
+      reason: reason || {}
+    };
+    errors.push(e);
+    return e;
+  };
+
+  // Get all errors
+  service.get = function() {
+    return errors;
+  }
+
+  return service;
+
+}
 
 }());
---
+++ test/unit/errors/errors.spec.js
@@ -0,0 +1,59 @@
+describe('4me.core.errors', function() {
+  beforeEach(module('4me.core.errors'));
+
+  var errors;
+
+  beforeEach(inject(function(_errors_) {
+    errors = _errors_;
+  }));
+
+  it('should have a proper API', function() {
+    errors.add.should.be.a('Function');
+    errors.get.should.be.a('Function');
+    errors.catch.should.be.a('Function');
+  });
+
+  describe('get', function() {
+    it('should return an array', function() {
+      errors.get().should.eql([]);
+    });
+  });
+
+  describe('add', function() {
+    it('should add a correctly formatted error', function() {
+      var e = {
+        sender: 'core',
+        type: 'critical',
+        message: 'Error',
+        reason: {message: 'test error'}
+      };
+
+      errors.add('core', 'critical', 'Error', {message: 'test error'})
+      .should.eql(e);
+      errors.get().should.eql([e]);
+    });
+  });
+
+  describe('catch', function() {
+    it('should add a correctly formatted error via a promise chain', function(done) {
+      // Setup a failing promise
+      var p = function() {
+        return new Promise(function(resolve, reject) {
+          reject(new Error('bla'));
+        });
+      };
+
+      p()
+      .then(function() {
+        (true).should.eql(0); // This should never happen
+      })
+      .catch(errors.catch('core', 'critical', 'Promise failed'))
+      .then(function() {
+        errors.get().length.should.eql(1);
+        errors.get()[0].message.should.eql('Promise failed');
+      })
+      .should.notify(done);
+    });
+  });
+
+});
