4f7eb0ab86e0068769304648f5d4b3310a2a53ac
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-07 15:54:55 +0100

    Add a chip with unread error counts

--- app/errors/errors.components.js
+++ app/errors/errors.components.js
@@ -13,19 +13,26 @@ var errorComponents = angular.module('4me.core.errors.components', [
   '4me.core.errors'
 ]);
 errorComponents.component('fmeErrorButton', {
-  retrict: 'E',
+  restrict: 'E',
+  controller: fmeErrorButtonController,
+  templateUrl: 'views/errors/fmeErrorButton.tpl.html'
 });
-
 errorComponents.component('fmeErrorList', {
   restrict: 'E',
-  controller: fmeErrorController,
+  controller: fmeErrorListController,
   templateUrl: 'views/errors/fmeErrorList.tpl.html'
 });
 
-fmeErrorController.$inject = ['errors'];
-function fmeErrorController(errors) {
+fmeErrorListController.$inject = ['errors'];
+function fmeErrorListController(errors) {
   var self = this;
   self.errors = errors.get();
+}
+
+fmeErrorButtonController.$inject = ['errors'];
+function fmeErrorButtonController(errors) {
+  var self = this;
+  self.unreadCount = errors.getUnreadCount();
 }
 
 }());
