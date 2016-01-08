4909da2e0535e5815d736744540ecd60eb8d7e96
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-08 23:31:14 +0100

    Add stub module

---
+++ organs/stub/main.js
@@ -0,0 +1,23 @@
+(function() {
+'use strict';
+/**
+ * @ngdoc overview
+ * @name 4me.ui.stub
+ * @description
+ * # 4me.ui.stub
+ *
+ * Stub organ, using 4me.core.ui hooks
+ */
+var m = angular
+  .module('4me.ui.stub', [
+      'ui.router',
+      '4me.core.partials',
+      '4me.core.config',
+      '4me.core.errors',
+      '4me.core.organs',
+      '4me.core.cwp',
+      '4me.core.sectors'
+  ]);
+
+
+}());
