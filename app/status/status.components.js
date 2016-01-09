5675384e8c48b50df67e31bf555c05a1e363595c
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-09 18:48:28 +0100

    Add status button

--- app/status/status.components.js
+++ app/status/status.components.js
@@ -13,5 +13,49 @@ var statusComponents = angular.module('4me.core.status.components', [
   '4me.core.status'
 ]);
 
+statusComponents.component('fmeStatusButton', {
+  restrict: 'E',
+  controller: fmeStatusButtonController,
+  templateUrl: 'views/status/fmeStatusButton.tpl.html'
+});
+
+fmeStatusButtonController.$inject = ['coreStatusService'];
+function fmeStatusButtonController(coreStatusService) {
+  var fmeStatusButton = this;
+
+  fmeStatusButton.getStatus = function() {
+    return coreStatusService.get();
+  };
+
+  fmeStatusButton.disableClick = function() {
+    return true;
+    return fmeStatusButton.getStatus().status === 'normal';
+  };
+
+  fmeStatusButton.getIcon = function() {
+    switch (fmeStatusButton.getStatus().status) {
+      case 'normal':
+        return 'check';
+      case 'warn':
+        return 'alert';
+      default:
+      case 'critical':
+        return 'close-circle';
+    }
+    return 'check';
+  };
+
+  fmeStatusButton.getClass = function() {
+    switch (fmeStatusButton.getStatus().status) {
+      case 'normal':
+        return '';
+      case 'warn':
+        return 'md-warn';
+      default:
+      case 'critical':
+        return 'md-accent';
+    }
+  };
+}
 
 }());
