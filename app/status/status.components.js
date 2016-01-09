a1c932b6224fb0e5ac97533335851d2438796699
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-09 18:10:21 +0100

    Add status component placeholder

---
+++ app/status/status.components.js
@@ -0,0 +1,17 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 4me.core.status.components
+ * @description
+ * # Status components
+ * Status components
+ */
+var statusComponents = angular.module('4me.core.status.components', [
+  '4me.core.lodash',
+  '4me.core.status'
+]);
+
+
+}());
--- app/status/status.js
+++ app/status/status.js
@@ -10,7 +10,8 @@
  * Meta module to include status components/services
  */
 angular.module('4me.core.status', [
-  '4me.core.status.services'
+  '4me.core.status.services',
+  '4me.core.status.components'
 ]);
 
 }());
