8ef46f82e91c0dd2230e180df0c568ce7e398eaf
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-09 14:56:20 +0100

    Add empty organ testing

---
+++ test/unit/organs/organs.services.spec.js
@@ -0,0 +1,15 @@
+describe('4me.core.organs.services', function() {
+  beforeEach(module('4me.core.organs.services'));
+
+  describe('mainOrganService', function() {
+    var mainOrganService;
+    var errors;
+    var notifications;
+
+    it('should pass', function() {
+      (true).should.be.true;
+    });
+
+
+  });
+});
