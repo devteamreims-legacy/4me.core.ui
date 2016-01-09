10216e7a91c84ca9cac50f4be66941042437f823
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-09 17:40:45 +0100

    Add very basic status stuff

--- app/status/status.services.js
+++ app/status/status.services.js
@@ -19,16 +19,35 @@ var statusServices = angular.module('4me.core.status.services', [
 
 statusServices.factory('coreStatusService', coreStatusService);
 
-coreStatusService.$inject = [];
-function coreStatusService() {
+coreStatusService.$inject = ['_'];
+/**
+ * This has the power to block the whole app
+ */
+function coreStatusService(_) {
   var service = {};
 
+  var _normalStatus = {
+    status: 'normal', // normal, warn, critical
+    message: '',
+    reasons: []
+  };
+
+  var status = _.clone(_normalStatus);
+
   service.get = function() {
-    return {
-      status: 'normal',
-      message: '',
-      reasons: []
-    };
+    return status;
+  };
+
+  service.recover = function() {
+    status = _.clone(_normalStatus);
+    return this.get();
+  };
+
+  service.escalate = function(newStatus, message, reasons) {
+    status.status = newStatus || 'warn';
+    status.message = message || 'Unknown reason !';
+    status.reasons = reasons || [];
+    return this.get();
   };
 
   return service;
--- test/unit/status/status.services.spec.js
+++ test/unit/status/status.services.spec.js
@@ -10,6 +10,36 @@ describe('4me.core.status.services', function() {
 
     it('should offer a proper API', function() {
       coreStatusService.get.should.be.a('Function');
+      coreStatusService.escalate.should.be.a('Function');
+      coreStatusService.recover.should.be.a('Function');
+    });
+
+    it('should present a normal status by default', function() {
+      var s = coreStatusService.get();
+      s.status.should.eql('normal');
+      s.message.should.eql('');
+      s.reasons.should.eql([]);
+    });
+
+    it('should be able to escalate', function() {
+      coreStatusService.escalate('warn', 'User message');
+
+      var s = coreStatusService.get();
+      s.status.should.eql('warn');
+      s.message.should.eql('User message');
+      s.reasons.should.eql([]);
+    });
+
+    it('should be able to resume to a normal status', function() {
+      coreStatusService.escalate('warn', 'User message');
+
+      coreStatusService.recover();
+
+      var s = coreStatusService.get();
+      s.status.should.eql('normal');
+      s.message.should.eql('');
+      s.reasons.should.eql([]);
+
     });
   });
 
