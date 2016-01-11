d8d02666ef23ab613f0952d702ece578361fa1c9
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-11 12:08:40 +0100

    Add find method to organs

--- test/unit/organs/organs.services.spec.js
+++ test/unit/organs/organs.services.spec.js
@@ -41,6 +41,16 @@ describe('4me.core.organs.services', function() {
       mainOrganService.getAll().should.eql([r]);
     });
 
+    it('should be able to find an organ', function() {
+      var r = mainOrganService.register(stubOrgan);
+      mainOrganService.find('stub').should.eql(r);
+    });
+
+    it('should return an empty object if unable to find said organ', function() {
+      expect(mainOrganService.find('stub')).to.be.undefined;
+      expect(mainOrganService.find()).to.be.undefined;
+    });
+
     it('should decorate the navigateTo callback', function() {
       var stubFunc = sinon.stub();
       var o = _.clone(stubOrgan);
