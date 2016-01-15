1168c53d9352c7183c25c3f567d4ea4d0d783651
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-15 21:50:18 +0100

    Add styles folders to watch task

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,8 @@ var cachebust = new CacheBuster();
 var config = {
     appFolder:      [
         'app/**/*',
+        'styles/**/*',
+        'organs/*/styles/**/*',
         'organs/*/app/**/*'
     ],
     sassFolder:     [
