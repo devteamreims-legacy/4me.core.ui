05e9c6278674991732cfd1b63d37475def592d45
Benjamin Beret <benjamin.beret@gmail.com>
2016-01-28 16:43:16 +0100

    Add tests, refactor some, still WIP

--- test/unit/sectors/sectors.mysectors.spec.js
+++ test/unit/sectors/sectors.mysectors.spec.js
@@ -1,8 +1,8 @@
-describe('4me.core.cwp.services', function() {
-  beforeEach(module('4me.core.cwp.services'));
+describe('4me.core.sectors.services', function() {
+  beforeEach(module('4me.core.sectors.services'));
 
-  describe('myCwp', function() {
-    var myCwp;
+  describe('mySector', function() {
+    var mySector;
     var $httpBackend;
     var $rootScope;
     var $q;
@@ -12,18 +12,19 @@ describe('4me.core.cwp.services', function() {
     var cwpInterceptor;
     var mainWebSocket;
     var backendResponse;
+    var myCwp;
 
     var resultsFromBackend = {
       getMine: {
-        id: 34,
+        cwpId: 34,
         name: 'P34',
         type: 'cwp',
         disabled: false
       }
     };
 
-    beforeEach(inject(function(_myCwp_, _$httpBackend_, _$rootScope_, _$q_, _ApiUrls_, _errors_, _cwpInterceptor_, _status_, _mainWebSocket_, _$cookies_) {
-      myCwp = _myCwp_;
+    beforeEach(inject(function(_mySector_, _$httpBackend_, _$rootScope_, _$q_, _ApiUrls_, _errors_, _cwpInterceptor_, _status_, _mainWebSocket_, _$cookies_, _myCwp_) {
+      mySector = _mySector_;
       $httpBackend = _$httpBackend_;
       $rootScope = _$rootScope_;
       $q = _$q_;
@@ -33,16 +34,19 @@ describe('4me.core.cwp.services', function() {
       status = _status_;
       mainWebSocket = _mainWebSocket_;
       $cookies = _$cookies_;
+      myCwp = _myCwp_;
+
+      myCwp.bootstrap = sinon.stub().resolves({id: 34});
     }));
 
     it('should present a proper API', function() {
-      myCwp.bootstrap.should.be.a('function');
-      myCwp.get.should.be.a('function');
-      myCwp.refresh.should.be.a('function');
+      mySector.bootstrap.should.be.a('function');
+      mySector.get.should.be.a('function');
+      mySector.refresh.should.be.a('function');
     });
 
     it('should return empty stuff when not bootstrapped', function() {
-      myCwp.get().should.eql({});
+      mySector.get().should.eql({});
     });
 
     it('should be able to be bootstrapped', function(done) {
@@ -50,7 +54,7 @@ describe('4me.core.cwp.services', function() {
         .when('GET', ApiUrls.mapping.rootPath + ApiUrls.mapping.cwp.getMine)
         .respond(resultsFromBackend.getMine);
 
-      myCwp.bootstrap()
+      mySector.bootstrap()
         .should.be.fulfilled
         .and.notify(done);
 
@@ -60,13 +64,11 @@ describe('4me.core.cwp.services', function() {
     describe('get', function() {
       // Prepare our backend
       beforeEach(function() {
-        cwpInterceptor.setId = sinon.stub();
-        $cookies.put = sinon.stub();
         $httpBackend
           .when('GET', ApiUrls.mapping.rootPath + ApiUrls.mapping.cwp.getMine)
           .respond(resultsFromBackend.getMine);
 
-        myCwp.bootstrap()
+        mySector.bootstrap()
         .should.be.fulfilled;
 
         $httpBackend.flush();
@@ -77,7 +79,7 @@ describe('4me.core.cwp.services', function() {
         $httpBackend.verifyNoOutstandingRequest();
       });
 
-      it('should return a properly formatted result', function() {
+      xit('should return a properly formatted result', function() {
         var c = myCwp.get();
         c.should.include.keys('id', 'name', 'type', 'disabled');
         c.id.should.be.a('number');
@@ -86,18 +88,18 @@ describe('4me.core.cwp.services', function() {
         c.disabled.should.be.a('boolean');
       });
 
-      it('should set a proper id in cwpInterceptor', function() {
+      xit('should set a proper id in cwpInterceptor', function() {
         cwpInterceptor.setId
         .should.have.been
         .calledWith(resultsFromBackend.getMine.id);
       });
 
-      it('should set a proper my-cwp-id cookie', function() {
+      xit('should set a proper my-cwp-id cookie', function() {
         $cookies.put.should.have.been.calledWith('my-cwp-id', resultsFromBackend.getMine.id);
       });
 
       describe('socket', function() {
-        it('should update with data from socket', function() {
+        xit('should update with data from socket', function() {
           $httpBackend.expectGET(ApiUrls.mapping.rootPath + ApiUrls.mapping.cwp.getMine);
           mainWebSocket.receive('cwp:refresh', {
             id: 34,
@@ -120,7 +122,7 @@ describe('4me.core.cwp.services', function() {
         errors.add = sinon.stub().returns({});
       });
 
-      it('should handle failure gracefully', function(done) {
+      xit('should handle failure gracefully', function(done) {
         myCwp.bootstrap()
         .then(function(res) {
           // This should not happen
